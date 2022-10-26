2f3d2e43dc791d62bae841b58e909aa36c917aad
Amul Gaurav <amulgaurav003@gmail.com>
2022-10-26 23:09:10 +0530

    Initial commit - created 'Band Name Generator'

---
+++ Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator :")
+city = input("In which city you were born?\n")
+nick_name = input("What is the name of your pet?\n")
+print("The name of your Band could be - " + city , nick_name)
