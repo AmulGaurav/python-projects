d70ea82a7102121d565641a1e8528f258fd6fc32
Amul Gaurav <amulgaurav003@gmail.com>
2022-10-31 20:42:24 +0530

    Added interpretations to the BMI Calculator

--- BMI Calculator/bmi_calculator.py
+++ BMI Calculator/bmi_calculator.py
@@ -7,4 +7,14 @@ weight = float(input("Enter your weight in kg : "))
 height = float(input("Enter your height in m : "))
 
 BMI = round((weight / height ** 2) , 1)
-print(BMI)
+
+if BMI < 18.5:
+    print(f"Your BMI is {BMI}, you are underweight.")
+elif BMI < 25:
+    print(f"Your BMI is {BMI}, you have a normal weight.")
+elif BMI < 30:
+    print(f"Your BMI is {BMI}, you are slightly overweight.")
+elif BMI < 35:
+    print(f"Your BMI is {BMI}, you are obese.")
+else:
+    print(f"Your BMI is {BMI}, you are clinically obese.")
