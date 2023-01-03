36a348ef5840fe2f1dbaffa4d120515a87d4f586
Amul Gaurav <amulgaurav003@gmail.com>
2023-01-03 20:47:57 +0530

    Add high score to the snake-game

--- snake-game/main.py
+++ snake-game/main.py
@@ -35,13 +35,13 @@ while game_is_on:
 
     # Detect collision with wall.
     if (snake.head.xcor() > 280) or (snake.head.xcor() < -280) or (snake.head.ycor() > 280) or (snake.head.ycor() < -280):
+        scoreboard.reset()
         game_is_on = False
-        scoreboard.game_over()
 
     # Detect collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
+            scoreboard.reset()
             game_is_on = False
-            scoreboard.game_over()
 
 screen.exitonclick()
--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -7,6 +7,8 @@ class Scoreboard(Turtle):
 
     def __init__(self) -> None:
         super().__init__()
+        with open("C:\\Users\\admin\\Desktop\\Python Projects\\snake-game\\data.txt") as data:
+            self.high_score = int(data.read())
         self.score = 0
         self.penup()
         self.hideturtle()
@@ -15,13 +17,23 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(arg=f"Score: {self.score} | High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_scoreboard(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            # self.high_score = self.score
+            with open("C:\\Users\\admin\\Desktop\\Python Projects\\snake-game\\data.txt", mode="w") as data:
+                data.write(f"{self.score}")
+            self.goto(0, -10)
+            self.write(arg=f"New High Score: {self.score}", align=ALIGNMENT, font=FONT)    
+        self.game_over()
+        
 
     def game_over(self):
         self.goto(0, 0)
-        self.write(arg="GAME OVER", align=ALIGNMENT, font=("Courier", 15, "normal"))
+        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
